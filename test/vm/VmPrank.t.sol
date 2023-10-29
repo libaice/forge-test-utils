58fcbd99e9c78a4720f67b6782d6674ef66d1626
root <libaice147@gmail.com>
2023-10-29 10:11:45 +0000

    add new foundry.toml configuration

---
+++ test/vm/VmPrank.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+
+contract VmPrankTest is Test {
+    function test_VmPrank() public {
+        uint input = 0;
+        // vm.prank();
+    }
+}
