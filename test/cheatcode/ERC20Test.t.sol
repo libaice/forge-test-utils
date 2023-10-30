b3436a2cee6b3ea6f417e0b2b5fd1d3918a10835
root <libaice147@gmail.com>
2023-10-30 09:57:12 +0000

    add erc20 test

--- test/cheatcode/ERC20Test.t.sol
+++ test/cheatcode/ERC20Test.t.sol
@@ -48,7 +48,7 @@ contract ERC20Test is Test {
             v,
             r,
             s
-        );  
+        );
 
         console.log(token.allowance(owner, spender));
         console.log(token.nonces(owner));
@@ -56,4 +56,208 @@ contract ERC20Test is Test {
         assertEq(token.allowance(owner, spender), 1e18);
         assertEq(token.nonces(owner), 1);
     }
+
+    function testRevert_ExpiredPermit() public {
+        SignUtils.Permit memory permit = SignUtils.Permit({
+            owner: owner,
+            spender: spender,
+            value: 1e18,
+            nonce: token.nonces(owner),
+            deadline: 1 days
+        });
+        bytes32 digest = signUtils.getTypedDataHash(permit);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, digest);
+        vm.warp(1 days + 1 seconds);
+
+        vm.expectRevert("PERMIT_DEADLINE_EXPIRED");
+        token.permit(
+            permit.owner,
+            permit.spender,
+            permit.value,
+            permit.deadline,
+            v,
+            r,
+            s
+        );
+    }
+
+    function testRevert_InvalidNonce() public {
+        SignUtils.Permit memory permit = SignUtils.Permit({
+            owner: owner,
+            spender: spender,
+            value: 1e18,
+            nonce: 1,
+            deadline: 1 days
+        });
+
+        bytes32 digest = signUtils.getTypedDataHash(permit);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, digest);
+        // vm.expectRevert("PERMIT_INVALID_NONCE");
+        vm.expectRevert("INVALID_SIGNER");
+
+        token.permit(
+            permit.owner,
+            permit.spender,
+            permit.value,
+            permit.deadline,
+            v,
+            r,
+            s
+        );
+    }
+
+    function testRevert_SignatureReplay() public {
+        SignUtils.Permit memory permit = SignUtils.Permit({
+            owner: owner,
+            spender: spender,
+            value: 1e18,
+            nonce: 0,
+            deadline: 1 days
+        });
+
+        bytes32 digest = signUtils.getTypedDataHash(permit);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, digest);
+
+        token.permit(
+            permit.owner,
+            permit.spender,
+            permit.value,
+            permit.deadline,
+            v,
+            r,
+            s
+        );
+
+        vm.expectRevert("INVALID_SIGNER");
+        token.permit(
+            permit.owner,
+            permit.spender,
+            permit.value,
+            permit.deadline,
+            v,
+            r,
+            s
+        );
+    }
+
+    function test_TransferFromLimitedPermit() public {
+        SignUtils.Permit memory permit = SignUtils.Permit({
+            owner: owner,
+            spender: spender,
+            value: 1e18,
+            nonce: 0,
+            deadline: 1 days
+        });
+        bytes32 digest = signUtils.getTypedDataHash(permit);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, digest);
+
+        token.permit(
+            permit.owner,
+            permit.spender,
+            permit.value,
+            permit.deadline,
+            v,
+            r,
+            s
+        );
+
+        vm.prank(spender);
+        token.transferFrom(owner, spender, 1e18);
+        
+        console.log(token.balanceOf(spender));
+
+        assertEq(token.balanceOf(owner), 0);
+        assertEq(token.balanceOf(spender), 1e18);
+        assertEq(token.allowance(owner, spender), 0);
+    }
+
+
+    function test_TransferFromMaxPermit() public {
+        SignUtils.Permit memory permit = SignUtils.Permit({
+            owner: owner,
+            spender: spender,
+            value: type(uint256).max,
+            nonce: 0,
+            deadline: 1 days
+        });
+        bytes32 digest = signUtils.getTypedDataHash(permit);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, digest);
+
+        token.permit(
+            permit.owner,
+            permit.spender,
+            permit.value,
+            permit.deadline,
+            v,
+            r,
+            s
+        );
+
+        vm.prank(spender);
+        token.transferFrom(owner, spender, 1e18);
+        
+        console.log(token.balanceOf(spender));
+
+        assertEq(token.balanceOf(owner), 0);
+        assertEq(token.balanceOf(spender), 1e18);
+        assertEq(token.allowance(owner, spender), type(uint256).max);
+    }
+
+    function testFail_InvalidAllowance() public {
+        SignUtils.Permit memory permit = SignUtils.Permit({
+            owner: owner,
+            spender: spender,
+            value: 5e17, // approve only 0.5 tokens
+            nonce: 0,
+            deadline: 1 days
+        });
+
+        bytes32 digest = signUtils.getTypedDataHash(permit);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, digest);
+
+        token.permit(
+            permit.owner,
+            permit.spender,
+            permit.value,
+            permit.deadline,
+            v,
+            r,
+            s
+        );
+
+        vm.prank(spender);
+        token.transferFrom(owner, spender, 1e18); // attempt to transfer 1 token
+    }
+
+    function testFail_InvalidBalance() public {
+        SignUtils.Permit memory permit = SignUtils.Permit({
+            owner: owner,
+            spender: spender,
+            value: 2e18, // approve 2 tokens
+            nonce: 0,
+            deadline: 1 days
+        });
+
+        bytes32 digest = signUtils.getTypedDataHash(permit);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, digest);
+
+        token.permit(
+            permit.owner,
+            permit.spender,
+            permit.value,
+            permit.deadline,
+            v,
+            r,
+            s
+        );
+
+        vm.prank(spender);
+        token.transferFrom(owner, spender, 2e18); // attempt to transfer 2 tokens (owner only owns 1)
+    }
+
+
+
+
 }
